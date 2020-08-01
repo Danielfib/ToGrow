a317363c9db5b50d7c0a432cdb49488cf11c798d
Daniel Filgueira <dfb2@cin.ufpe.br>
2020-08-01 12:32:14 -0300

    Making basic rewind behaviour

---
+++ Rewind/Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Translate(Vector3.forward * 8.0f * Time.deltaTime * Input.GetAxis("Vertical"));
+        transform.Rotate(Vector3.up * 200.0f * Time.deltaTime * Input.GetAxis("Horizontal"));
+    }
+}
---
+++ Rewind/Assets/Scripts/TimeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    public GameObject player;
+
+    public ArrayList playerPositions;
+
+    void Start()
+    {
+        playerPositions = new ArrayList();
+    }
+
+    void FixedUpdate()
+    {
+        playerPositions.Add(player.transform.position);
+    }
+
+    public void SpawnPlayerAndReverse()
+    {
+        StartCoroutine(ReverseCoroutine());
+    }
+
+    private IEnumerator ReverseCoroutine()
+    {
+        ArrayList positions = this.playerPositions;
+        GameObject playerClone = Instantiate(player);
+        playerClone.GetComponent<Player>().enabled = false;
+
+        for (var i = positions.Count - 1; i > 0; i--)
+        {
+            playerClone.transform.position = (Vector3)positions[i];
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
