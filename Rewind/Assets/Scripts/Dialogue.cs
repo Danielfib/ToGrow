18d8c4cef3dc9422e6563ac52cf4c59d54f7b234
Daniel Filgueira <dfb2@cin.ufpe.br>
2020-08-03 23:25:16 -0300

    Basic dialogue behaviour

---
+++ Rewind/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typeSpeed;
+
+    public GameObject continueButton;
+
+    private AudioSource audioSource;
+
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(Type());
+    }
+
+    IEnumerator Type()
+    {
+        continueButton.SetActive(false);
+
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+
+        continueButton.SetActive(true);
+    }
+
+    public void NextSentence()
+    {
+        audioSource?.Play();
+        if(index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        } else
+        {
+            //dialogue complete
+            continueButton.SetActive(false);
+            textDisplay.text = "";
+        }
+    }
+}
---
+++ Rewind/Assets/Scripts/Goal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Goal : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Win!");
+        }
+    }
+}
